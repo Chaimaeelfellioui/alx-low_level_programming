0bc2219496ec9f7bf4f9c293a871b4940ff5d362
chaimaeelfellioui <chaimae1307@gmail.com>
2023-03-21 15:00:01 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,18 @@
  * print_alphabet - prints lowercase alphabet
  * Return: 0
  */
+
 void print_alphabet(void)
 {
-char alpha;
-alpha = 'a';
-while (alpha <= 'z')
-{
-_putchar(alpha);
-alpha++;
-}
-_putchar('\n');
+
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
 }

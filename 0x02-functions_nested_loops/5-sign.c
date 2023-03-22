1e97abdf14cb374cc074c7bf6c8e7d5f88cf05be
chaimaeelfellioui <chaimae1307@gmail.com>
2023-03-22 05:27:31 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @k: the nunber to be chevked for sign
+ *
+ * Return: 1 if sign is positive
+ * 0 if sign is zero
+ * -1 if sign is negative
+ */
+
+int print_sign(int k)
+{
+	if (k > 0)
+	{
+		_putchar('+');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

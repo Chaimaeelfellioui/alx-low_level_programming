18fea921f465480f83a01f2b462740202772837b
chaimaeelfelliuoi <chaimae1307@gmail.com>
2023-04-19 04:03:53 -0700

    0-print_name.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,9 +13,9 @@ int x, y;
 for (y = 0; y < size; y++)
 {
 for (x = 0; x < size; x++)
-_putchar('#');
+       _putchar('#');
 putchar('\n');
 }
 if (size < 1)
-_putchar('\n');
+        _putchar('\n');
 }
